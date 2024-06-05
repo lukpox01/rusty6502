585a682cdd995b4cce67a67bfb392368d0f546b1
Lukas Polacek <lukiy223344@gmail.com>
2024-06-05 20:12:24 +0200

    LDA - IMM ZP modes

---
+++ src/instructions.rs
@@ -0,0 +1,11 @@
+
+use crate::Byte;
+pub const LDA_IMM: Byte = 0xA9;
+pub const LDA_ZP: Byte = 0xA5;
+// pub const LDA_ZPX: Byte = 0xB5;
+// pub const LDA_ABS: Byte = 0xAD;
+// pub const LDA_ABSX: Byte = 0xBD;
+// pub const LDA_ABSY: Byte = 0xB9;
+// pub const LDA_INDX: Byte = 0xA1;
+// pub const LDA_INDY: Byte = 0xB1;
+
