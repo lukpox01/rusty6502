3148e8b967c968b8b6cf1153f5018bf03dd53774
Lukas Polacek <lukiy223344@gmail.com>
2024-06-07 20:02:10 +0200

    completed whole LDA instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,13 +3,13 @@ use crate::memory::Memory;
 
 #[allow(non_snake_case, unused)]
 pub struct Flags {
-    Carry: bool,
-    Zero: bool,
-    InterruptDisable: bool,
-    DecimalMode: bool,
-    Break: bool,
-    Overflow: bool,
-    Negative: bool,
+    pub Carry: bool,
+    pub Zero: bool,
+    pub InterruptDisable: bool,
+    pub DecimalMode: bool,
+    pub Break: bool,
+    pub Overflow: bool,
+    pub Negative: bool,
 }
 
 #[allow(non_snake_case)]
@@ -83,17 +83,32 @@ impl CPU {
         data
     }
 
-    pub fn execute(&mut self, mem: &mut Memory) {
+    pub fn execute(&mut self, mem: &mut Memory, cycle: &mut Cycle) {
         let opcode = self.fetch_byte(mem);
         match opcode {
             instructions::LDA::IMM => {
-                self.handle_LDA_IMM(mem, &mut 2);
+                self.handle_LDA_IMM(mem, cycle);
             }
             instructions::LDA::ZP => {
-                self.handle_LDA_ZP(mem, &mut 3);
+                self.handle_LDA_ZP(mem, cycle);
             }
             instructions::LDA::ZPX => {
-                self.handle_LDA_ZPX(mem, &mut 4)
+                self.handle_LDA_ZPX(mem, cycle)
+            }
+            instructions::LDA::ABS => {
+                self.handle_LDA_ABS(mem, cycle)
+            }
+            instructions::LDA::ABSX => {
+                self.handle_LDA_ABSX(mem, cycle)
+            }
+            instructions::LDA::ABSY => {
+                self.handle_LDA_ABSY(mem, cycle)
+            }
+            instructions::LDA::INDX => {
+                self.handle_LDA_INDX(mem, cycle)
+            }
+            instructions::LDA::INDY => {
+                self.handle_LDA_INDY(mem, cycle)
             }
             _ => panic!("Unknown opcode: {:X}", opcode),
         }
@@ -128,6 +143,82 @@ impl CPU {
         let mut value = self.read_byte(mem, address);
         *cycle -= 1;
         value += self.X;
+        *cycle -= 1;
+        self.A = value;
+        *cycle -= 1;
+        self.set_flags_LDA()
+    }
+
+    fn handle_LDA_ABS(&mut self, mem: &mut Memory, cycle: &mut Cycle) {
+        let address: Word = self.fetch_word(mem);
+        *cycle -= 1;
+        let mut value = self.read_byte(mem, address);
+        *cycle -= 1;
+        self.A = value;
+        *cycle -= 1;
+        self.set_flags_LDA()
+    }
+
+    fn handle_LDA_ABSX(&mut self, mem: &mut Memory, cycle: &mut Cycle) {
+        let base_address: Word = self.fetch_word(mem);
+        *cycle -= 1;
+        let address = base_address + self.X as Word;
+        *cycle -= 1;
+        if (base_address >> 8) & 0xFF != (address>>8)&0xFF{
+            *cycle -= 1
+        }
+        let value = self.read_byte(mem, address);
+        *cycle -= 1;
+        self.A = value;
+        *cycle -= 1;
+        self.set_flags_LDA()
+    }
+
+    fn handle_LDA_ABSY(&mut self, mem: &mut Memory, cycle: &mut Cycle) {
+        let base_address: Word = self.fetch_word(mem);
+        *cycle -= 1;
+        let address = base_address + self.Y as Word;
+        *cycle -= 1;
+        if (base_address >> 8) & 0xFF != (address>>8)&0xFF{
+            *cycle -= 1
+        }
+        let value = self.read_byte(mem, address);
+        *cycle -= 1;
+        self.A = value;
+        *cycle -= 1;
+        self.set_flags_LDA()
+    }
+
+    fn handle_LDA_INDX(&mut self, mem: &mut Memory, cycle: &mut Cycle) {
+        let mut address: Word = (0x0000 | self.fetch_byte(mem)) as Word;
+        *cycle -= 1;
+        address = address + self.X as Word;
+        *cycle -= 1;
+        let lo = self.read_byte(mem, address);
+        let hi = self.read_byte(mem, address+0x01);
+        *cycle -= 1;
+        let effective_address = ((hi as u16) << 8) | lo as u16;
+        *cycle -= 1;
+        let value = self.read_byte(mem, effective_address);
+        *cycle -= 1;
+        self.A = value;
+        *cycle -= 1;
+        self.set_flags_LDA()
+    }
+
+    fn handle_LDA_INDY(&mut self, mem: &mut Memory, cycle: &mut Cycle) {
+        let zp_address: Word = (0x0000 | self.fetch_byte(mem)) as Word;
+        *cycle -= 1;
+        let lo = self.read_byte(mem, zp_address);
+        let hi = self.read_byte(mem, zp_address+0x01);
+        *cycle -= 1;
+        let base_address = (((hi as u16) << 8) | lo as u16) ;
+        let effective_address = base_address + self.Y as u16;
+        *cycle -= 1;
+        if (base_address >> 8) & 0xFF != (effective_address>>8)&0xFF{
+            *cycle -= 1
+        }
+        let value = self.read_byte(mem, effective_address as Word);
         *cycle -= 1;
         self.A = value;
         *cycle -= 1;
--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,7 @@
 
 use crate::memory::Memory;
 use crate::cpu::CPU;
-use crate::instructions;
+use crate::{instructions};
 
 #[allow(non_snake_case)]
 #[test]
@@ -15,9 +15,18 @@ fn LDA_ZPX_CAN_LOAD_AND_OFFSET() {
     mem.data[0x0010] = 0x11;
     cpu.X = 0x5;
 
-    cpu.execute(&mut mem);
+    cpu.execute(&mut mem, &mut 4);
 
-    assert_eq!(cpu.A, 0x16)
+    assert_eq!(cpu.A, 0x16);
+
+    // Flags
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
 }
 
 #[allow(non_snake_case)]
@@ -31,9 +40,18 @@ fn LDA_ZP_CAN_LOAD() {
     mem.data[0xFFFD] = 0x10;
     mem.data[0x0010] = 0x11;
 
-    cpu.execute(&mut mem);
+    cpu.execute(&mut mem, &mut 3);
 
-    assert_eq!(cpu.A, 0x11)
+    assert_eq!(cpu.A, 0x11);
+
+    // Flags
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
 }
 
 #[allow(non_snake_case)]
@@ -46,7 +64,229 @@ fn LDA_IMM_CAN_LOAD() {
     mem.data[0xFFFC] = instructions::LDA::IMM;
     mem.data[0xFFFD] = 0x10;
 
-    cpu.execute(&mut mem);
+    cpu.execute(&mut mem, &mut 2);
 
-    assert_eq!(cpu.A, 0x10)
+    assert_eq!(cpu.A, 0x10);
+
+    // Flags
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_FLAG_NEGATIVE() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::IMM;
+    mem.data[0xFFFD] = 0x80;
+
+    cpu.execute(&mut mem, &mut 2);
+
+    assert_eq!(cpu.A, 0x80);
+
+    // Flags
+    assert_eq!(cpu.Status.Negative, true);
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_FLAG_ZERO() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::IMM;
+    mem.data[0xFFFD] = 0x00;
+
+    cpu.execute(&mut mem, &mut 2);
+
+    assert_eq!(cpu.A, 0x00);
+
+    // Flags
+    assert_eq!(cpu.Status.Zero, true);
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_ABS_CAN_LOAD() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::ABS;
+    mem.data[0xFFFD] = 0x80;
+    mem.data[0xFFFE] = 0x80;
+    mem.data[0x8080] = 0x15;
+
+    cpu.execute(&mut mem, &mut 4);
+
+    assert_eq!(cpu.A, 0x15);
+
+    // Flags
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_ABSX_CAN_LOAD() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::ABSX;
+    mem.data[0xFFFD] = 0x80;
+    mem.data[0xFFFE] = 0x80;
+    mem.data[0x8081] = 0x15;
+    cpu.X = 0x01;
+
+    cpu.execute(&mut mem, &mut 4);
+
+    assert_eq!(cpu.A, 0x15);
+
+    // Flags
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_ABSY_CAN_LOAD() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::ABSY;
+    mem.data[0xFFFD] = 0x81;
+    mem.data[0xFFFE] = 0x81;
+    mem.data[0x8183] = 0x0B;
+    cpu.Y = 0x02;
+
+    cpu.execute(&mut mem, &mut 4);
+
+    assert_eq!(cpu.A, 0x0B);
+
+    // Flags
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_INDX_CAN_LOAD() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::INDX;
+    mem.data[0xFFFD] = 0x1E;
+    mem.data[0x0020] = 0x05;
+    mem.data[0x0021] = 0x10;
+    mem.data[0x1005] = 0x0F;
+    cpu.X = 0x02;
+
+    cpu.execute(&mut mem, &mut 6);
+
+    assert_eq!(cpu.A, 0x0F);
+
+    // Flags
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_INDY_CAN_LOAD() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::INDY;
+    mem.data[0xFFFD] = 0x20;
+    mem.data[0x0020] = 0x30;
+    mem.data[0x0021] = 0x40;
+    mem.data[0x4035] = 0x22;
+    cpu.Y = 0x05;
+
+    cpu.execute(&mut mem, &mut 5);
+
+    assert_eq!(cpu.A, 0x22);
+
+    // Flags
+    assert_eq!(cpu.Status.Negative, false);
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn LDA_INDY_CROSSED_PAGE() {
+    let mut mem = Memory::new();
+    let mut cpu = CPU::new();
+    cpu.reset();
+
+    mem.data[0xFFFC] = instructions::LDA::INDY;
+    mem.data[0xFFFD] = 0x20;
+    mem.data[0x0020] = 0xFF;
+    mem.data[0x0021] = 0x30;
+    mem.data[0x3101] = 0x80;
+    cpu.Y = 0x02;
+
+    cpu.execute(&mut mem, &mut 6);
+
+    assert_eq!(cpu.A, 0x80);
+
+    // Flags
+    assert_eq!(cpu.Status.Negative, true);
+    assert_eq!(cpu.Status.Zero, false);
+    assert_eq!(cpu.Status.Overflow, false);
+    assert_eq!(cpu.Status.Carry, false);
+    assert_eq!(cpu.Status.DecimalMode, false);
+    assert_eq!(cpu.Status.InterruptDisable, false);
+    assert_eq!(cpu.Status.Break, false);
 }
